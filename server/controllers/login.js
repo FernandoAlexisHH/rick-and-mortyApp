49efb26978d2b7d45241c13379bc6161eeaf2787
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2023-12-18 00:34:14 -0600

    Agregando mas test

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -2,7 +2,6 @@ const usuario = require('../utils/users')
 
 function login(req, res) {
     const { email, password } = req.query
-    console.log(usuario[0].email,usuario[0].password, email,password);
     if(email === usuario[0].email && password === usuario[0].password){
        return res.status(200).json({
             access: true
--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,17 +1,34 @@
 const app = require('../app');
 const session = require('supertest');
 const agent = session(app);
+const user = require('../utils/users')
 
 describe('Test de RUTAS',() => {
-    describe('GET/rickandmorty/character/:id',()=> {
+    describe('GET/rickandmorty/character/:id',() => {
         it('Responde con status: 200', async() => {
             await agent.get('/rickandmorty/character/1').expect(200);
         })
         it('Responde un objeto con la propiedades "id", "name", "species", "gender", "status", "origin" e "image"',
         async () => {
             const res = await agent.get('/rickandmorty/character/1')
-            console.log(res.body);
             expect(res.body).toHaveProperty("id","name","species","gender","status","origin","image")
+        })
+        it('Si hay un error responde con status: 500',async () => {
+            const res = await agent.get("/rickandmorty/character/5000")
+            expect(res.statusCode).toBe(500)
+
+        })
+    });
+
+    describe('GET /rickandmorty/login',() => {
+        it('Si email y password son correctos enviara un objeto con la propiedad access en true', async () => {
+            const res = await agent.get(`/rickandmorty/login?email=${user[0].email}&password=${user[0].password}`)
+            expect(res.body).toHaveProperty("access",true)
+        })
+        it('Si el email y password son erroneos enviara un objeto con la propiedad access false', async() => {
+            const res = await agent.get(`/rickandmorty/login?email="usuarioerroneo@hotmail.com"&password="1234"`)
+            expect(res.body).toHaveProperty("access", false)
+
         })
     })
 
